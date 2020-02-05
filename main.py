d76c1b834ac2e4ead72c78ae66ce5c783dce6350
Kusergit <60576271+Kusergit@users.noreply.github.com>
2020-02-05 18:44:03 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_api
+import config
+
+
+token = config.TOKEN
+vk_session = vk_api.VkApi(token=token)
+session_api = vk_session.get_api()
+longpoll = VkLongPoll(vk_session)
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.from_user and not event.from_me:
+            print('Пришло какое-то сообщение')
+            print('Текст сообщения: ' + str(event.text))
+            vk_session.method('messages.send', {'user_id' : event.user_id, 'message': 'Я бот помошник python джедаев!', 'random_id':0})
+    if event.type == VkEventType.USER_TYPING:
+        vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Я знаю что ты набираешь текст!))$', 'random_id':0})
